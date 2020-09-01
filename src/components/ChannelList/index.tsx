d32e0681c3759c588f52f1da2bd14e0b47148f0e
Aldrin <aldrinjunior@gmail.com>
2020-08-31 23:04:51 -0300

    ChannelList

---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+const ChannelList: React.FC = () => {
+  return (
+    <Container>
+      <Text>ChannelList</Text>
+    </Container>
+  );
+};
+
+export default ChannelList;
--- src/components/StreamList/index.tsx
+++ src/components/StreamList/index.tsx
@@ -1,15 +1,64 @@
 import React from "react";
 import { Text } from "react-native";
 
-import streamThumbnail from "../../images/stream_thumnail.jpg";
+import streamThumbnail from "../../images/stream_thumbnail.jpg";
 
-import { Container } from "./styles";
+import {
+  List,
+  StreamContainer,
+  StreamRow,
+  StreamThumbnail,
+  StreamColumn,
+  StreamHeader,
+  StreamAvatar,
+  StreamUsername,
+  StreamDescription,
+  StreamCategory,
+  TagRow,
+  TagView,
+  TagText,
+} from "./styles";
 
 const StreamList: React.FC = () => {
+  const StreamItem = () => (
+    <StreamContainer>
+      <StreamThumbnail source={streamThumbnail} />
+
+      <StreamColumn>
+        <StreamRow>
+          <StreamHeader>
+            <StreamAvatar />
+            <StreamUsername numberOfLines={1}>rodz_oficial</StreamUsername>
+          </StreamHeader>
+
+          <StreamDescription numberOfLines={1}>
+            Front-end com Next.js, Chakra UI e GraphQL
+          </StreamDescription>
+
+          <StreamCategory numberOfLines={1}>
+            Science & Technology
+          </StreamCategory>
+        </StreamRow>
+
+        <TagRow>
+          <TagView>
+            <TagText>Portuguese</TagText>
+          </TagView>
+          <TagView>
+            <TagText>Web Development</TagText>
+          </TagView>
+        </TagRow>
+      </StreamColumn>
+    </StreamContainer>
+  );
+
   return (
-    <Container>
-      <Text>StreamList</Text>
-    </Container>
+    <List>
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+      <StreamItem />
+    </List>
   );
 };
 
